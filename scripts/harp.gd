d833b3c3690c512cbafd6f9c53695c83bd2eb11a
Jacson <jacson.gold@student.onslow.school.nz>
2024-06-21 18:07:50 +1200

    Test commit

--- scripts/harp.gd
+++ scripts/harp.gd
@@ -1,7 +1,7 @@
 extends Area2D
 # Defining of whether or not Lyre has been collected
 @onready var variables = %Variables
-
+var lyre_pickup = false
 func _on_body_entered(body):
 	lyre_pickup = true
 	queue_free()
