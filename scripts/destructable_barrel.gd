172812122e37019600e04513d0a7913e79ba03e0
Jacson <jacson.gold@student.onslow.school.nz>
2024-06-27 12:06:08 +1200

    fixing-file-locations

--- scripts/destructable_barrel.gd
+++ scripts/destructable_barrel.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 var health = 1
 
 @onready var animated_sprite = $AnimatedSprite2D
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	animated_sprite.play("idle")
@@ -15,7 +16,7 @@ func _ready():
 
 func barrel_death():
 	animated_sprite.play("destruction")
-
+	
 func take_damage(damage):
 	health -= damage
 	if health > 0:
