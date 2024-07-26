9881ed8012686093d9c57e2d696a1c07782aca1f
Jacson <jacson.gold@student.onslow.school.nz>
2024-07-26 19:53:00 +1200

    intro-cutscene-intro

--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -2,5 +2,5 @@ extends Control
 
 
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/game.tscn")
+	get_tree().change_scene_to_file("res://scenes/intro_cutscene.tscn")
 	pass # Replace with function body.
