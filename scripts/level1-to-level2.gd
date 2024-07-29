c6ad5622110820808987deb883ec4c825c3ce178
Jacson <jacson.gold@student.onslow.school.nz>
2024-07-29 21:36:18 +1200

    Sound effect and level2 implementation

--- scripts/level1-to-level2.gd
+++ scripts/level1-to-level2.gd
@@ -1,7 +1,8 @@
 extends Area2D
 
 func _on_body_entered(body):
-	print("Hi!!")
+	print("First Level Completed")
+	get_tree().change_scene_to_file("res://scenes/level2.tscn")
 	pass # Replace with function body.
 
 # Called when the node enters the scene tree for the first time.
