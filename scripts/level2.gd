4a161b7565c51481fac7697354b8824be83b182b
Jacson <jacson.gold@student.onslow.school.nz>
2024-08-11 17:14:58 +1200

    settings menu theme implemented

--- scripts/level2.gd
+++ scripts/level2.gd
@@ -4,7 +4,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Global.lyre_pickup = false
+	Global.lyre_pickup = true
 	pass # Replace with function body.
 
 
